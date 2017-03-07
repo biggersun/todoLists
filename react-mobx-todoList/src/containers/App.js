baa9d5d74fda1555aa5d2b13b66fcd9817cee953
biggersun <2659528347@qq.com>
2017-03-08 00:43:39 +0800

    complete demo

--- react-mobx-todoList/src/containers/App.js
+++ react-mobx-todoList/src/containers/App.js
@@ -1,5 +1,16 @@
 import React, { Component, PropTypes } from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 
+class Todo {
+  id = Math.random();
+  @observable title = ''
+  constructor(title) {
+      this.title = title;
+  }
+}
+
+@observer
 class App extends Component {
   constructor(props){
     super(props);
@@ -7,28 +18,43 @@ class App extends Component {
       inputValue: ""
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleChange(e){
     this.setState({
       inputValue: e.target.value
     });
   }
+  handleClick(){
+    this.props.todoList.todoList.push(
+      new Todo(this.state.inputValue)
+    );
+  }
   render() {
     return (
       <div>
         <input type="text" value={this.state.inputValue} onChange={this.handleChange}/>
-        
+        <button onClick={this.handleClick}>添加</button>
+        {this.props.todoList.todoList.map(todo => <ListView key={todo.id} title={todo.title}/>)}
       </div>
     );
   }
 }
 
+const ListView = (props) => {
+  let title = props.title;
+  console.log(props);
+  return (
+    <div>{title}</div>
+    )
+}
+
 const propTypes = {
-  listData: PropTypes.array.isRequired
+  todoList: PropTypes.array.isRequired
 };
 
 App.defaultProps = {
-  listData: [1,2]
+  todoList: [1,2]
 }
 
 export default App
--- react-mobx-todoList/src/index.js
+++ react-mobx-todoList/src/index.js
@@ -2,10 +2,18 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { observable, computed } from 'mobx'
+import { observer } from 'mobx-react'
 
 import App from './containers/App';
 
+class TodoList {
+  @observable todoList = []
+}
+
+const store = new TodoList()
+
 render(
-  <App />,
+  <App todoList={store} />,
   document.getElementById('root'),
 )
